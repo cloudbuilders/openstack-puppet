b261a00444582b30fc75bcfb00a5fa39e3d06c17
rpedde <ron.pedde@rackspace.com>
2011-06-17 17:52:51 -0500

    use init status for glance

--- modules/glance/manifests/service.pp
+++ modules/glance/manifests/service.pp
@@ -1,8 +1,9 @@
 class glance::service {
   $glance_services = [ "glance-api", "glance-registry" ]
   service { $glance_services:
-    ensure  => running,
-    enable  => true,
-    require => Class["glance::install"]
+    ensure    => running,
+    enable    => true,
+    hasstatus => true,
+    require   => Class["glance::install"]
   }
 }
