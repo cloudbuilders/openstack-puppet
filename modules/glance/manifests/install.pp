3f161502fa947fee57758ae90f5f9d7a6a2b530b
rpedde <ron.pedde@rackspace.com>
2011-06-20 09:47:20 -0500

    Fix permissions on glance log file so it will start up

--- modules/glance/manifests/install.pp
+++ modules/glance/manifests/install.pp
@@ -4,5 +4,18 @@ class glance::install {
   package { $glance_packages:
     ensure => present
   }
+
+  file { "/var/log/glance":
+    ensure => directory,
+    owner  => "glance",
+    mode   => 0755
+  }
+
+  file { "/var/log/glance/api.log":
+    ensure => present,
+    owner  => "glance",
+    mode   => "600",
+    require => File["/var/log/glance"]
+  }
 }
   
