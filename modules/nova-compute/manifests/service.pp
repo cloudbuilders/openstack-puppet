b536d583516dd249565c387b66446ca99903068b
rpedde <ron.pedde@rackspace.com>
2011-06-20 11:18:07 -0500

    Fix dependancy order on nova compute services

--- modules/nova-compute/manifests/service.pp
+++ modules/nova-compute/manifests/service.pp
@@ -2,12 +2,14 @@ class nova-compute::service {
   service { "nova-compute":
     ensure    => running,
     enable    => true,
-    subscribe => File["/etc/nova/nova.conf"]
+    subscribe => File["/etc/nova/nova.conf"],
+    require   => Class["nova-compute::install"]
   }
 
   service { "nova-network":
     ensure    => running,
     enable    => true,
-    subscribe => File["/etc/nova/nova.conf"]
+    subscribe => File["/etc/nova/nova.conf"],
+    require   => Class["nova-compute::install"]
   }
 }
