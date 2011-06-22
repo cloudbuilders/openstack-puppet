f218592b32df906f9980bd824bdeb10958194ae4
Vishvananda Ishaya <vishvananda@gmail.com>
2011-06-22 14:47:44 -0700

    try using apt::source

--- modules/nova-common/manifests/install.pp
+++ modules/nova-common/manifests/install.pp
@@ -2,23 +2,23 @@ class nova-common::install {
   $nova_common_packages = [ "nova-common", "nova-doc", "python-nova", "python-eventlet" ]
   $nova_common_misc     = [ "euca2ools", "unzip" ]
 
-  apt::key { "460DF9BE":
-    ensure => present,
-  }
-
-  apt::sources_list {"rcb":
-    ensure => present,
-    content => "deb http://devpackages.ansolabs.com maverick main",
-    require => Apt::Key["460DF9BE"]
+  apt::source { "rcb":
+    location => "http://devpackages.ansolabs.com",
+    release => "maverick",
+    repos => "main",
+    required_packages => "debian-keyring debian-archive-keyring",
+    key => "460DF9BE",
+    key_server => "keyserver.ubuntu.com",
+    pin => "-10"
   }
 
   package { $nova_common_packages:
     ensure  => latest,
-    require => Apt::Sources_list["rcb"]
+    require => Apt::Source["rcb"]
   }
 
   package { $nova_common_misc:
     ensure  => present,
-    require => Apt::Sources_list["rcb"]
+    require => Apt::Source["rcb"]
   }
 }
