77c3209da038f43a839e4d45e33d3eb84b289d84
rpedde <ron@pedde.com>
2011-06-22 18:43:05 -0500

    note that swift services have a status command

--- modules/swift-storage/manifests/service.pp
+++ modules/swift-storage/manifests/service.pp
@@ -12,24 +12,27 @@ class swift-storage::service {
   $swift_object_server_services = [ "swift-object", "swift-object-replicator",
                                     "swift-object-auditor", "swift-object-updater" ]
   service { $swift_object_server_services:
-    ensure => running,
-    enable => true
+    ensure    => running,
+    enable    => true,
+    hasstatus => true
   }
 
   # account services
   $swift_account_server_services = [ "swift-account", "swift-account-replicator",
                                      "swift-account-auditor", "swift-account-reaper" ]
   service { $swift_account_server_services:
-    ensure => running,
-    enable => true
+    ensure    => running,
+    enable    => true,
+    hasstatus => true
   }
 
   # container services
   $swift_container_server_services = [ "swift-container", "swift-container-replicator",
                                        "swift-container-auditor", "swift-container-updater" ]
   service { $swift_container_server_services:
-    ensure => running,
-    enable => true
+    ensure    => running,
+    enable    => true,
+    hasstatus => true
   }
 }
   
