cc5d488fc257007f86864855e35ee7ca72a66cd5
Anthony Young <sleepsonthefloor@gmail.com>
2011-06-23 18:15:20 -0700

    add in paste config

--- modules/nova-infra/manifests/install.pp
+++ modules/nova-infra/manifests/install.pp
@@ -5,5 +5,15 @@ class nova-infra::install {
   package { $nova_infra_packages:
     ensure => latest
   }
+
+  file { "nova-api-paste.ini":
+    path => "/etc/nova/nova-api-paste.ini",
+    source  => "puppet:///modules/nova-infra/nova-api-paste.ini",
+    ensure => present,
+    require => [
+      Package["nova-api"]
+    ]
+  }
+
 }
   
