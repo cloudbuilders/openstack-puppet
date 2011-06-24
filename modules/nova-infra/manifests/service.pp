60a41e7357142205a58baa16301b33c1b4ece13f
Jesse Andrews <anotherjesse@gmail.com>
2011-06-23 18:58:50 -0700

    Enable in /etc/defaults

--- modules/nova-infra/manifests/service.pp
+++ modules/nova-infra/manifests/service.pp
@@ -4,6 +4,12 @@ class nova-infra::service {
   service { $nova_infra_services:
     ensure    => running,
     enable    => true,
+    require   => File["nova-default"],
     subscribe => File["/etc/nova/nova.conf"]
+  }
+  
+  file { "nova-default":
+    path => "/etc/default/nova-common",
+    content => "ENABLED=1"
   }
 }
