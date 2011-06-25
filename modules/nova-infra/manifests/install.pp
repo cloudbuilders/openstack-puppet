24fdc7faf3938e504c1d35590b0db9942189d432
Anthony Young <sleepsonthefloor@gmail.com>
2011-06-25 13:58:16 -0700

    shouldn't have to add the dep here :(  nova-novnc should install with nova-vncproxy, but it doesn't

--- modules/nova-infra/manifests/install.pp
+++ modules/nova-infra/manifests/install.pp
@@ -1,6 +1,7 @@
 class nova-infra::install {
   # not all of these are probably requires on an infra box
-  $nova_infra_packages = [ "nova-api", "nova-objectstore", "nova-scheduler", "nova-network", "nova-vncproxy" ]
+  # FIXME shouldn't have to specify nova-novnc here
+  $nova_infra_packages = [ "nova-api", "nova-objectstore", "nova-scheduler", "nova-network", "nova-vncproxy", "nova-novnc" ]
 
   package { $nova_infra_packages:
     ensure => latest
