f6a3aec4892daca0ec0d177808a19f6bd0ad0a87
Vishvananda Ishaya <vishvananda@gmail.com>
2011-06-27 07:30:20 -0700

    remove fix iptables

--- modules/nova-compute/manifests/service.pp
+++ modules/nova-compute/manifests/service.pp
@@ -7,15 +7,4 @@ class nova-compute::service {
     subscribe => File["/etc/nova/nova.conf"],
     require   => Class["nova-compute::install"]
   }
-
-  exec { "fix_iptables.sh":
-    command => "/root/fix_iptables.sh",
-    user => "root",
-    path => "/usr/bin:/bin:/sbin:/usr/sbin",
-    subscribe => Service["nova-compute"],
-    require => [
-      File["/root/fix_iptables.sh"]
-    ]
-  }
-
 }
