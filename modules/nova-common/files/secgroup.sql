da5493f21c34a43b5663902d1668b7b2f44d2c8a
Vishvananda Ishaya <vishvananda@gmail.com>
2011-06-27 07:34:33 -0700

    add secgroup.sql

---
+++ modules/nova-common/files/secgroup.sql
@@ -0,0 +1,2 @@
+INSERT INTO `security_groups` VALUES ('2011-06-27 14:25:56',NULL,NULL,0,1,'default','default','admin','admin');
+INSERT INTO `security_group_rules` VALUES ('2011-06-27 14:25:56',NULL,NULL,0,1,1,'tcp',1,65535,'0.0.0.0/0',NULL),('2011-06-27 14:26:03',NULL,NULL,0,2,1,'udp',1,65535,'0.0.0.0/0',NULL),('2011-06-27 14:26:21',NULL,NULL,0,3,1,'icmp',-1,-1,'0.0.0.0/0',NULL);
