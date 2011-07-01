c1190740b5c00d29ef93af363af18c4ff6fc112a
rpedde <ron@pedde.com>
2011-07-01 12:26:23 -0500

    Add multi-cluster configs, fallback to test cluster with old bleep

--- lib/enc.py
+++ lib/enc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import json
+import os
 import re
 import sys
 import urllib2
@@ -8,10 +9,10 @@ import urllib2
 # default kick options, override in /etc/puppet/enc.yaml
 #
 config = {
-    'kick_uri':  'http://192.168.122.2:5000',
-    'kick_user': 'admin',
-    'kick_pass': 'supersecret'
-    }
+    'kick_uri':  'http://<bleep server>:<bleep port>',
+    'kick_user': '<admin user>',
+    'kick_pass': '<admin pass>'
+}
 
 with open('/etc/puppet/enc.yaml') as f:
     yaml_config = json.load(f)
@@ -23,7 +24,7 @@ hardware_uri = config['kick_uri'] + '/admin/hardware/view'
 
 auth_handler = urllib2.HTTPBasicAuthHandler()
 auth_handler.add_password(realm = 'kick dingus',
-                          uri = hardware_uri,
+                          uri = config['kick_uri'],
                           user = config['kick_user'],
                           passwd = config['kick_pass'])
                           
@@ -39,13 +40,43 @@ oskick_details = json.load(r)
 # the oskick data, but we don't have multi-tenant support
 # there yet, so we'll dummy it up
 
+hostname = sys.argv[1] #.split('.')[0]
+
+machine_info = [x for x in oskick_details['hardware'] if x['hostname'] == hostname ]
+if not machine_info:
+    print "Can't find machine info"
+    sys.exit(1)
+    
+cluster_id = machine_info[0]['cluster_id']
+
+# now, pull the short name of the cluster
+
+cluster_uri = config['kick_uri'] + '/admin/clusters/view/%s' % cluster_id
+req = urllib2.Request(cluster_uri, None, { 'Accept': 'application/json' })
+
 cluster_name = 'test'
+
+try:
+    r = urllib2.urlopen(req)
+    cluster_name = json.load(r)['short_name']
+except urllib2.URLError, e:
+    if e.code != 404:
+        sys.exit(1)
+
+# print oskick_details
 
 cluster_details = {}
 
-with open('/etc/puppet/%s.yaml' % cluster_name) as f:
-    cluster_details = json.load(f)
+try:
+    cluster_description = '/etc/puppet/%s.yaml' % cluster_name
+    fd = open(cluster_description)
+except Exception as e:
+    print 'Error opening %s' % cluster_description
+    print e
+    sys.exit(1)
 
+cluster_details = json.load(fd)
+fd.close()
 
 # walk through the cluster config, find all matches for
 # each role.
@@ -55,26 +86,27 @@ machines_by_role = {}
 
 for role, rolematch in cluster_details['cluster'].items():
     for hwinfo in oskick_details['hardware']:
-        hostname = hwinfo['hostname']
-        
-        if hostname and re.match(rolematch, hostname):
-            if not roles_by_machine.has_key(hostname):
-                roles_by_machine[hostname] = []
-            roles_by_machine[hostname].append(role)
+        host = hwinfo['hostname']
+
+        if host and re.match(rolematch, host):
+            if not roles_by_machine.has_key(host):
+                roles_by_machine[host] = []
+            roles_by_machine[host].append(role)
 
             if not machines_by_role.has_key(role):
                 machines_by_role[role] = []
 
-            if hostname not in machines_by_role[role]:
-                machines_by_role[role].append(hostname)
+            if host not in machines_by_role[role]:
+                machines_by_role[role].append(host)
             
 # Now generate the roles list
 enc_manifest = { 'classes': [], 'parameters': {} }
 
-hostname = sys.argv[1] #.split('.')[0]
+print "looking for roles for %s" % hostname
 
 if not roles_by_machine.has_key(hostname):
     # not found..
+    print "No roles!"
     sys.exit(1)
 
 for role in roles_by_machine[hostname]:
