5b05c120393c2ae8aee7ed401aa9e9b0dc2eed2c
Anthony Young <sleepsonthefloor@gmail.com>
2011-07-27 17:25:30 -0700

    allow munin to be accessed from apache

--- modules/munin/manifests/install.pp
+++ modules/munin/manifests/install.pp
@@ -1,5 +1,19 @@
 class munin::install {
   package { "munin":
-    ensure => present
+    ensure => present,
+    require => [
+      Service["apache2"]
+    ],
+  }
+
+  file { "/etc/apache2/conf.d/munin":
+    ensure => link,
+    target => "/etc/munin/apache.conf",
+    require => [
+      Package["munin"]
+    ],
+    notify => [
+      Service["apache2"]
+    ]
   }
 }
