919dc97effc56c9dbcccaf45671b188842f964ef
Jesse Andrews <anotherjesse@gmail.com>
2011-07-27 22:49:49 -0700

    nova.conf should be owned by nova

--- modules/nova-common/manifests/config.pp
+++ modules/nova-common/manifests/config.pp
@@ -2,7 +2,7 @@ class nova-common::config {
   # set up the nova.conf
   file { "/etc/nova/nova.conf":
     ensure  => present,
-    owner   => "root",
+    owner   => "nova",
     group   => "nogroup",
     mode    => 0660,
     content => template("nova-common/nova.conf.erb")
