0107557b5a98b5a57330d9a8b6f8ca2df72d06f2
Will Kelly <the.william.kelly@gmail.com>
2011-08-03 04:10:32 -0500

    added use_virtio_net option

--- modules/nova-common/manifests/config.pp
+++ modules/nova-common/manifests/config.pp
@@ -7,4 +7,11 @@ class nova-common::config {
     mode    => 0660,
     content => template("nova-common/nova.conf.erb")
   }
+  file { "/usr/share/pyshared/nova/virt/libvirt.xml.template":
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => 0644,
+    content => template("nova-common/libvirt.xml.template.erb")
+  }
 }
