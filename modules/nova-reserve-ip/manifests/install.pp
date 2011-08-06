1fa6cb4f78df33e7b7b643dbce1def024314160b
Vishvananda Ishaya <vishvananda@gmail.com>
2011-08-05 17:19:41 -0700

    do some funky mysql for reserving ips

--- modules/nova-reserve-ip/manifests/install.pp
+++ modules/nova-reserve-ip/manifests/install.pp
@@ -5,9 +5,9 @@ class nova-reserve-ip::install {
   }
 
   exec { "reserve_ip":
-    command     => "mysql #",
+    command     => "HOST=`hostname`; mysql -u${mysql_nova_user} -p${mysql_nova_password} nova -e \"update fixed_ips set host='$HOST' where address='${host_vmnet_ip}'\"",
     path        => [ "/bin", "/usr/bin" ],
-    not_if      => "mysql #",
+    unless      => "test `hostname` == `mysql -u${mysql_nova_user} -p${mysql_nova_password} nova -e \"select host from fixed_ips where address='${host_vmnet_ip}'\"`",
   }
 }
 
