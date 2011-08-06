e841a70c10a396b4974c1bf8f1c4f797b8b7363c
Vishvananda Ishaya <vishvananda@gmail.com>
2011-08-05 18:33:14 -0700

    fix reserve ip

--- modules/nova-reserve-ip/manifests/install.pp
+++ modules/nova-reserve-ip/manifests/install.pp
@@ -1,13 +1,13 @@
 class nova-reserve-ip::install {
 
-  package { "mysql":
+  package { "mysql-client":
     ensure  => latest,
   }
 
   exec { "reserve_ip":
-    command     => "HOST=`hostname`; mysql -u${mysql_nova_user} -p${mysql_nova_password} nova -e \"update fixed_ips set host='$HOST' where address='${host_vmnet_ip}'\"",
+    command     => "HOST=`hostname`; mysql -h${mysql_vip} -u${mysql_nova_user} -p${mysql_nova_password} nova -e \"update fixed_ips set host='$HOST' where address='${host_vmnet_ip}'\"",
     path        => [ "/bin", "/usr/bin" ],
-    unless      => "test `hostname` == `mysql -u${mysql_nova_user} -p${mysql_nova_password} nova -e \"select host from fixed_ips where address='${host_vmnet_ip}'\"`",
+    unless      => "test `hostname` == `mysql -h{mysql_vip} -u${mysql_nova_user} -p${mysql_nova_password} nova -e \"select host from fixed_ips where address='${host_vmnet_ip}'\"`",
   }
 }
 
