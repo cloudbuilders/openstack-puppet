1f4c15769f35344c4591ef4e1bac0d62b01c2ceb
Vishvananda Ishaya <vishvananda@gmail.com>
2011-08-05 18:39:50 -0700

    hard-code nova user

--- modules/nova-reserve-ip/manifests/install.pp
+++ modules/nova-reserve-ip/manifests/install.pp
@@ -5,9 +5,9 @@ class nova-reserve-ip::install {
   }
 
   exec { "reserve_ip":
-    command     => "mysql -h${mysql_vip} -u${mysql_nova_user} -p${mysql_nova_password} nova -e \"update fixed_ips set host='`hostname`' where address='${host_vmnet_ip}'\"",
+    command     => "mysql -h${mysql_vip} -unova -p${mysql_nova_password} nova -e \"update fixed_ips set host='`hostname`' where address='${host_vmnet_ip}'\"",
     path        => [ "/bin", "/usr/bin" ],
-    unless      => "test `hostname` == `mysql -h{mysql_vip} -u${mysql_nova_user} -p${mysql_nova_password} nova -e \"select host from fixed_ips where address='${host_vmnet_ip}'\"`",
+    unless      => "test `hostname` == `mysql -h{mysql_vip} -unova -p${mysql_nova_password} nova -e \"select host from fixed_ips where address='${host_vmnet_ip}'\"`",
   }
 }
 
