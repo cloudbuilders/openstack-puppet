37d13334eb9e2623d5f59248fba0633e90bfc00c
Anthony Young <sleepsonthefloor@gmail.com>
2011-08-06 20:42:29 -0700

    make sure mysql-client is installed before calling mysql

--- modules/nova-reserve-ip/manifests/install.pp
+++ modules/nova-reserve-ip/manifests/install.pp
@@ -8,6 +8,7 @@ class nova-reserve-ip::install {
     command     => "mysql -h${mysql_vip} -unova -p${mysql_nova_password} nova -e \"update fixed_ips set host='`hostname`' where address='${host_vmnet_ip}'\"",
     path        => [ "/bin", "/usr/bin" ],
     unless      => "test `hostname` == `mysql -h{mysql_vip} -unova -p${mysql_nova_password} nova -e \"select host from fixed_ips where address='${host_vmnet_ip}'\"`",
+    require     => Package["mysql-client"],
   }
 }
 
