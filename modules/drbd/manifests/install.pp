04169a80da1c65532e457be1c85f1c6c449b6779
Anthony Young <sleepsonthefloor@gmail.com>
2011-08-06 18:51:50 -0700

    lower drbd runlevel to 19

--- modules/drbd/manifests/install.pp
+++ modules/drbd/manifests/install.pp
@@ -2,5 +2,12 @@ class drbd::install {
   package { 'drbd8-utils':
     ensure => installed
   }
+
+  exec { "fix_drbd_runlevel":
+    command     =>  "update-rc.d -f drbd remove && update-rc.d drbd defaults 19",
+    path        => [ "/sbin", "/usr/sbin" ],
+    unless      => "stat /etc/rc3.d/S19drbd",
+    require => Package['drbd8-utils']
+  }
 }
 
