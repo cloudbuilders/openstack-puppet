4ee8712ff1c645b1aa80a067905df5058f6b08e8
Anthony Young <sleepsonthefloor@gmail.com>
2011-08-06 18:53:41 -0700

    fix path for stat command

--- modules/drbd/manifests/install.pp
+++ modules/drbd/manifests/install.pp
@@ -5,7 +5,7 @@ class drbd::install {
 
   exec { "fix_drbd_runlevel":
     command     =>  "update-rc.d -f drbd remove && update-rc.d drbd defaults 19",
-    path        => [ "/sbin", "/usr/sbin" ],
+    path        => [ "/sbin", "/usr/sbin", "/usr/bin/" ],
     unless      => "stat /etc/rc3.d/S19drbd",
     require => Package['drbd8-utils']
   }
