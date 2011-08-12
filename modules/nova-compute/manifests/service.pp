75f009a34825b3c1696332bcbf0e0785b0b1e7fe
Vishvananda Ishaya <vishvananda@gmail.com>
2011-08-12 16:29:31 -0700

    add force to remove iptables lock

--- modules/nova-compute/manifests/service.pp
+++ modules/nova-compute/manifests/service.pp
@@ -2,7 +2,7 @@ class nova-compute::service {
   service { "nova-compute":
     ensure    => running,
     enable    => true,
-    start     => "rm /var/lock/nova/nova-iptables.lock.lock; start nova-compute",
+    start     => "rm -f /var/lock/nova/nova-iptables.lock.lock; start nova-compute",
     hasrestart=> false,
     subscribe => File["/etc/nova/nova.conf"],
     require   => Class["nova-compute::install"]
