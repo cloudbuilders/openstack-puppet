31d02f4c895968da8ed0da1eff0974845883eaa2
Anthony Young <sleepsonthefloor@gmail.com>
2011-08-13 09:31:22 -0700

    add explicit apt-get update dependency

--- modules/rcb-common/manifests/install.pp
+++ modules/rcb-common/manifests/install.pp
@@ -10,7 +10,15 @@ class rcb-common::install {
     repos => "main",
     key => "460DF9BE",
     key_server => "keyserver.ubuntu.com",
-    pin => "1"
+    pin => "1",
+    notify => Exec["apt-update"]
   }
+
+  exec { "apt-update":
+    command => "/usr/bin/apt-get update",
+    require => Apt::Source["rcb"],
+    refreshonly => true
+  }
+
 
 }
