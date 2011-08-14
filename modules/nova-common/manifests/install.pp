004e3520da20c318361270193854530df5ea1fda
Vishvananda Ishaya <vishvananda@gmail.com>
2011-08-13 19:00:11 -0700

    require newest novaclient

--- modules/nova-common/manifests/install.pp
+++ modules/nova-common/manifests/install.pp
@@ -1,11 +1,11 @@
 class nova-common::install {
-  $nova_common_packages = [ "nova-common", "nova-doc", "python-nova", "python-mysqldb", "python-eventlet" ]
+  $nova_common_packages = [ "nova-common", "nova-doc", "python-nova", "python-mysqldb", "python-eventlet", "python-novaclient" ]
 
   package { $nova_common_packages:
     ensure  => latest,
     require => Apt::Source["rcb"]
   }
-  
+
   file { "nova-default":
     path => "/etc/default/nova-common",
     content => "ENABLED=1",
