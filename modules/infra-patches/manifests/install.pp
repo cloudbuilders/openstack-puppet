683fa5d8e328cd3192bd6eea5fb3cc121298ff94
Vishvananda Ishaya <vishvananda@gmail.com>
2011-09-28 02:15:45 -0700

    typo in package name

--- modules/infra-patches/manifests/install.pp
+++ modules/infra-patches/manifests/install.pp
@@ -3,7 +3,7 @@ class infra-patches::install {
   file { "/usr/share/pyshared/django_openstack/api.py":
     source  => "puppet:///modules/infra-patches/api.py",
     backup  => true,
-    require => Package["dash"],
+    require => Package["openstack-dashboard"],
     notify  => Service["apache2"]
   }
 
