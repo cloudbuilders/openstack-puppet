041d8f01b4d62aebf92abe75f239fe2cb9815b07
root <root@ash-sysimg002.scea.com>
2011-10-10 23:45:03 +0000

    templatize initial_images to include keystone auth token for initial image import

--- modules/glance/files/initial_images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo -u nova wget http://images.ansolabs.com/tty.tgz -O /tmp/tty.tgz
-sudo -u nova mkdir /tmp/images
-sudo -u nova tar -C /tmp/images -zxf /tmp/tty.tgz
-
-sudo -u nova nova-manage image convert /tmp/images
